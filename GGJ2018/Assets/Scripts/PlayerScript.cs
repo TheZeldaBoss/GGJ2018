fd6e5f395aa03555af164e1f23aa65c17b1e76ba
Patalano <clement.patalano@gmail.com>
2018-01-26 23:08:41 +0100

    première itération

---
+++ GGJ2018/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        this.GetComponent<Rigidbody>().drag = 20f;
+        this.transform.Translate(x, 0, 0);
+    }
+}
