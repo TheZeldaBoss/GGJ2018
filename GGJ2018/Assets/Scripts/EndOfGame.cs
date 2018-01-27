78c9472cd017cec8a8b62760986772dbf7de5078
Benoit Tachet <benoit.tachet@epitech.eu>
2018-01-27 18:50:40 +0100

    ee

---
+++ GGJ2018/Assets/Scripts/EndOfGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndOfGame : MonoBehaviour {
+
+    public bool isMenu, isQuit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        if (isMenu)
+        {
+            SceneManager.LoadScene("benoit");
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ GGJ2018/Assets/Scripts/EndOfLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour {
+    public GameObject endOfLevelWindow;
+	// Use this for initialization
+	void Start () {
+        endOfLevelWindow.SetActive(false);
+	}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        endOfLevelWindow.SetActive(true);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
