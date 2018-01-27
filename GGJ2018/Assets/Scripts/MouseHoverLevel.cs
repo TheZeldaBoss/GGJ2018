0e7e3523a0f5db2076cc49ed4695906d3a53f996
Benoit Tachet <benoit.tachet@epitech.eu>
2018-01-27 15:15:44 +0100

    all menus done

---
+++ GGJ2018/Assets/Scripts/MouseHoverLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoverLevel : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        transform.Rotate(0, 3, 0);
+    }
+
+    void OnMouseExit()
+    {
+        transform.Rotate(0, -3, 0);
+    }
+}
---
+++ GGJ2018/Assets/Scripts/MouseHoverMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoverMenu : MonoBehaviour {
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        transform.Rotate(0, 3, 0);
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        transform.Rotate(0, -3, 0);
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
